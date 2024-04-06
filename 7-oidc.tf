d15e822012c77271d300913776a3e6dc9962117a
Yash Kale <yashkale46@gmail.com>
2024-04-06 18:34:57 +0530

    OIDC Variables Added

---
+++ 7-oidc.tf
@@ -0,0 +1,26 @@
+data "aws_partition" "current" {}
+
+# Resource: AWS IAM Open ID Connect Provider
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
+  thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+
+  tags = merge(
+    {
+      Name = "${var.cluster_name}"
+    },
+    local.tags
+  )
+}
+
+output "aws_iam_openid_connect_provider_arn" {
+  description = "AWS IAM Open ID Connect Provider ARN"
+  value = aws_iam_openid_connect_provider.oidc_provider.arn 
+}
+
+# Output: AWS IAM Open ID Connect Provider
+output "aws_iam_openid_connect_provider_extract_from_arn" {
+  description = "AWS IAM Open ID Connect Provider extract from ARN"
+   value = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
+}
--- outputs.tf
+++ outputs.tf
@@ -43,7 +43,6 @@ output "cluster_primary_security_group_id" {
   value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
 }
 
-
 # Node Group - Public
 
 output "node_group_public_id" {
