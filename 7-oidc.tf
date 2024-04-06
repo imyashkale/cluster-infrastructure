2773824f6a772d9d7373a7cd73cd484dc9d0073f
Yash Kale <yashkale46@gmail.com>
2024-04-06 18:36:39 +0530

    OIDC Variables Added

--- 7-oidc.tf
+++ 7-oidc.tf
@@ -4,7 +4,7 @@ data "aws_partition" "current" {}
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
   thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
-  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
 
   tags = merge(
     {
