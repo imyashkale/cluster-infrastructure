25bbd5e4aded97f0c8a3968fa937afd4211b09e0
Yash Kale <yashkale46@gmail.com>
2024-05-04 12:35:59 +0530

    Enable Ingress

--- monitoring/prometheus.tf
+++ monitoring/prometheus.tf
@@ -19,11 +19,6 @@ resource "helm_release" "prometheus" {
   }
 
   set {
-    name  = "ingress.annotations.\\alb\\.ingress\\.kubernetes\\.io/listen-ports"
-    value = jsonencode([{ HTTPS = 443 }, { HTTP = 80 }])
-  }
-
-  set {
     name  = "ingress.annotations\\.alb\\.ingress\\.kubernetes\\.io/certificate-arn"
     value = aws_acm_certificate.acm_cert.arn
   }
